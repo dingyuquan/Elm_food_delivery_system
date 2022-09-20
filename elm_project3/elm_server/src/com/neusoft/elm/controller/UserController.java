ea90267b0a6156508c2f491db9e745cbbb5ce6c4
guoxuantju <2904910649@qq.com>
2022-09-20 17:04:46 +0800

    项目三第一部分后端

--- elm_project3/elm_server/src/com/neusoft/elm/controller/UserController.java
+++ elm_project3/elm_server/src/com/neusoft/elm/controller/UserController.java
@@ -6,6 +6,14 @@ import com.neusoft.elm.po.User;
 
 public class UserController {
 	
+	public Object getUserByIdByPass(HttpServletRequest request) throws Exception{
+		 String userId = request.getParameter("userId");
+		 String password = request.getParameter("password");
+		 UserService service = new UserServiceImpl();
+		 User user = service.getUserByIdByPass(userId, password);
+		 return user;
+	}
+	
 	public Object login(HttpServletRequest request) {
 		String username = request.getParameter("username");
 		System.out.println(username);
