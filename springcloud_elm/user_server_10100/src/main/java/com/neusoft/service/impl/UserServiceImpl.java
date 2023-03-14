ff078f53517891ae3e88f1584a8266f0241c4072
dyq <dingyuquan58@gmail.com>
2023-03-14 14:03:12 +0800

    user-server

---
+++ springcloud_elm/user_server_10100/src/main/java/com/neusoft/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.neusoft.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.neusoft.mapper.UserMapper;
+import com.neusoft.po.User;
+//import com.neusoft.service.ScoreService;
+import com.neusoft.service.UserService;
+//import com.neusoft.service.VirtualWalletService;
+import com.neusoft.util.CommonUtil;
+//import com.neusoft.vo.ScoreVO;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserMapper userMapper;
+	
+//	@Autowired
+//	private VirtualWalletService virtualWalletService;
+	
+//	@Autowired
+//	private ScoreService scoreService;
+
+	@Override
+	public User getUserByIdByPass(User user) {
+
+		return userMapper.getUserByIdByPass(user);
+	}
+
+	@Override
+	public int getUserById(String userId) {
+		return userMapper.getUserById(userId);
+	}
+
+	@Override
+	public int saveUser(User user) {
+		int res = userMapper.saveUser(user);
+		virtualWalletService.creatWalletByUserId(user.getUserId());
+		ScoreVO scoreVO = new ScoreVO();
+		scoreVO.setChannelId(2);
+		scoreVO.setCreateDate(CommonUtil.getCurrentDate());
+		scoreVO.setExpireDate(CommonUtil.getNextData(30));
+		scoreVO.setUserId(user.getUserId());
+		scoreVO.setCredit(100);
+		scoreService.saveCredit(scoreVO);
+		return res;
+	}
+
+
+}
